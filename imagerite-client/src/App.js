1e04bb71b045dc5f5dabb509e66a530b16bb9901
Scott McFadden <scott@mdashconsulting.com>
2018-11-21 22:12:47 -0600

    squawk works

---
+++ imagerite-client/src/App.js
@@ -0,0 +1,25 @@
+
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className='container'>
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="#" className="brand-logo">Logo</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li>a  </li>
+                        <li>b</li>
+                        <li>cc</li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+  }
+}
+
+export default App;
